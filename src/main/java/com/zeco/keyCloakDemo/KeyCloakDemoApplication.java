16eb486a4b26f6042accd3740e1fa2745197c807
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2024-03-18 08:25:15 +0100

    initial commit

---
+++ src/main/java/com/zeco/keyCloakDemo/KeyCloakDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zeco.keyCloakDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyCloakDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyCloakDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeco/keyCloakDemo/KeyCloakDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeco.keyCloakDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyCloakDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
